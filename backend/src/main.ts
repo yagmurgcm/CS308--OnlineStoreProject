2beeda94fca823de5039b9b6f06126f5e15ed7b5
Yagmur Gecim <yagmur.gecim@sabanciuniv.edu>
2025-10-23 15:09:47 +0300

    Add /products endpoint, enable CORS, and update database + frontend folders

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 💡 CORS aktif et
+  app.enableCors({
+    origin: 'http://localhost:3001', // Frontend'in adresi
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
